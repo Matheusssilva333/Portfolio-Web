f2b2f4ad08cc8e91d932cddc9cf5abd54d22ad13
Matheus de Souza Silva <matheus1882014@outlook.com>
2026-02-06 19:30:16 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react',
+import React from 'react'
 import NeuralBackground from './components/NeuralBackground',
 import AIAssistant from './components/AIAssistant',
 import { PROJECTS, SKILLS, EXPERIENCE } from './constants.tsx',
