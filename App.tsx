ba25636484586bd3555b7380ac3ae0b10f68e4be
Matheus Silva <matheus1882014@outlook.com>
2026-02-10 12:17:13 -0300

    corrigindo link do meu perfil do linkedin que não estava funcionando

--- App.tsx
+++ App.tsx
@@ -207,11 +207,11 @@ const App: React.FC = () => {
               GitHub
             </a>
             <a
-              href="www.linkedin.com/in/matheus-silva-0b177b333"
+              href="https://www.linkedin.com/in/matheus-silva-0b177b333/"
               target="_blank"
               rel="noopener noreferrer"
               className="px-8 py-4 bg-gradient-to-r from-cyan-500 to-blue-600 rounded-xl font-bold hover:shadow-lg hover:shadow-cyan-500/30 transition-all duration-300"
-            >
+              >
               LinkedIn
             </a>
           </div>
